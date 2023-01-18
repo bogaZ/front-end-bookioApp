cc31adbd292e8004990bce506a95a7a24fac3f23
bogaZ <bagusnurahmad7@gmail.com>
2023-01-18 21:29:36 +0700

    update api.dart

--- lib/services/api.dart
+++ lib/services/api.dart
@@ -5,7 +5,7 @@ import 'package:http/http.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class Network {
-  final String _url = 'http://192.168.239.245:8000';
+  final String _url = 'http://127.0.0.1:8000';
   String get url => _url;
   //final String _url = 'http://192.168.39.245:8000/api';
   // 192.168.1.2 is my IP, change with your IP address
